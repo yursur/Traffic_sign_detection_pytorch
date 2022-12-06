b888c4c769af7a5875974d1b765736e0827d07ca
Yuriy Surnakov <yurasurnakov@gmail.com>
2022-12-06 23:49:16 +0300

    edit roots

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ BATCH_SIZE = 15
 DETECTION_THRESHOLD = 0.8
 
 #ROOTS
-ROOT_IMAGE = "detection/rtsd-d3-frames"
-ROOT_GT = "detection/rtsd-d3-gt"
-SAVE_MODEL_ROOT = "Traffic_sign_detection_pytorch/output/train/models"
-SAVE_PLOTS_ROOT = "Traffic_sign_detection_pytorch/output/train/plots"
-TEST_IMAGE_ROOT = "Traffic_sign_detection_pytorch/input"
-SAVE_IMAGE_TEST = "Traffic_sign_detection_pytorch/output/test"
+ROOT_IMAGE = "C:/Users/yuras/Projects/Signs/data/detection/rtsd-d3-frames"
+ROOT_GT = "C:/Users/yuras/Projects/Signs/data/detection/rtsd-d3-gt"
+SAVE_MODEL_ROOT = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/output/train/models"
+SAVE_PLOTS_ROOT = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/output/train/plots"
+TEST_IMAGE_ROOT = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/input"
+SAVE_IMAGE_TEST = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/output/test"
 
 #save models after every n epochs
 EPOCHS_SAVE_MODEL = 2

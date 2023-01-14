7d1d3275b98522941354affa52104b3775f19d2a
Yuriy Surnakov <yurasurnakov@gmail.com>
2023-01-14 19:18:06 +0300

    cosmetic

--- classification/config_classification.py
+++ classification/config_classification.py
@@ -5,21 +5,21 @@ and paths, which are used in the project.
 import torch
 
 ## DEVICE cuda OR cpu
-# DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-DEVICE = torch.device('cpu')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# DEVICE = torch.device('cpu')
 
 ## HYPER-PARAMETERS FOR TRAINING
 NUM_EPOCHS = 50
-BATCH_SIZE = 1
+BATCH_SIZE = 15
 
 ## PATHS
 IMAGES_PATH = "C:/Users/yuras/Projects/Signs/data/detection/rtsd-d3-frames"
 GT_PATH = "C:/Users/yuras/Projects/Signs/data/detection/rtsd-d3-gt"
-SAVE_MODEL_PATH = "../detection/output/train/models"
-SAVE_PLOTS_PATH = "../detection/output/train/plots"
-INFERENCE_IMAGE_PATH = "/input/inference"
+SAVE_MODEL_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/classification/output/train/models"
+SAVE_PLOTS_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/classification/output/train/plots"
+INFERENCE_IMAGE_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/input/inference"
 TEST_IMAGE_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/input/test"
-SAVE_IMAGE_PATH = "../detection/output/test"
+SAVE_IMAGE_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/classification/output/test"
 
 ## Save model and plot during training after every n epochs
 EPOCHS_SAVE_MODEL = 2

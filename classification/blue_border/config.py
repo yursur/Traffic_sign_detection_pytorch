73d0c63d8969e931b067fcbc1a23f08fa886a428
Yuriy Surnakov <yurasurnakov@gmail.com>
2023-02-01 23:07:36 +0300

    BATCH_SIZE

--- classification/blue_border/config.py
+++ classification/blue_border/config.py
@@ -1,1 +1,13 @@
+# LIST OF CLASSES OF BLUE_BORDER SUBCLASS
 CLASSES = ['5_16', '7_3', '7_2', '7_12', '7_4', '7_11', '7_7', '5_18', '7_5', '7_6', '7_1']
+
+# HYPER-PARAMETERS FOR TRAINING
+NUM_EPOCHS = 25
+BATCH_SIZE = 15
+
+# PATHS
+SAVE_MODEL_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/classification/blue_border/output/models"
+SAVE_PLOTS_PATH = "C:/Users/yuras/Projects/Traffic_sign_detection_pytorch/classification/blue_border/output/plots"
+
+# SAVE PLOTS EVERY 'EPOCHS_SAVE_PLOTS' EPOCH
+EPOCHS_SAVE_PLOTS = 2

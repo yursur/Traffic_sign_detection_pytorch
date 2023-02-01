111a7f83a070da737042d025ac4b596dea3b2ee8
Yuriy Surnakov <yurasurnakov@gmail.com>
2023-02-02 00:18:04 +0300

    data_transforms added

--- classification/blue_border/datasets.py
+++ classification/blue_border/datasets.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import torch
+from torchvision import transforms as T
 from torch.utils.data import Dataset
 from PIL import Image
 
@@ -59,32 +60,57 @@ class classification_dataset(Dataset):
         label = self.labels[idx]
 
         if self.transform:
-            pass
+            img = self.transform(img)
 
-        print(f"ИТОГО ДЛЯ №{idx}:\n    img = {img}\n    label = {label}\n____________________________________________________________")
+        # print(f"ИТОГО ДЛЯ №{idx}:\n    img = {img}\n    label = {label}\n____________________________________________________________")
         return img, label
 
     def __len__(self):
         return len(self.imgs)
 
 
+# Transformations for datasets
+data_transforms = {
+    'train': T.Compose([
+        T.Resize(size=(225,225)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.3677, 0.3948, 0.4517], std=[0.2350, 0.2200, 0.2068])
+    ]),
+    'val': T.Compose([
+        T.Resize(size=(225,225)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.3677, 0.3948, 0.4517], std=[0.2350, 0.2200, 0.2068])
+    ])
+}
+
 # Get numbers_to_classes Dataframe for using it in num_to_class function.
-# This datafraim contains links between each of the class numbers and its real class.
+# This dataframe contains links between each of the class numbers and its real class.
 nums_to_classes_df = pd.read_csv(DATASETS_PATH+'/numbers_to_classes.csv')
 
 # Creating classification train and test datasets
 train_dataset = classification_dataset(root_dataset=DATASETS_PATH,
-                                                      train=True)
+                                       train=True,
+                                       transform=data_transforms['train'])
 test_dataset = classification_dataset(root_dataset=DATASETS_PATH,
-                                      train=False)
+                                      train=False,
+                                      transform=data_transforms['val'])
 
 # Creating dataloaders for classification datasets
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=BATCH_SIZE,
-                                           shuffle=True)
+                                           batch_size=len(train_dataset),
+                                           shuffle=False)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=BATCH_SIZE)
 
 print(f"Number of samples in BLUE_BORDER train dataset: {len(train_dataset)}")
 print(f"Number of samples in BLUE_BORDER test dataset: {len(test_dataset)}")
 
+
+# def mean_std(loader):
+#   images, labels = next(iter(loader))
+#   # shape of images = [b,c,w,h]
+#   mean, std = images.mean([0,2,3]), images.std([0,2,3])
+#   return mean, std
+#
+# mean, std = mean_std(train_loader)
+# print("mean and std: \n", mean, std)

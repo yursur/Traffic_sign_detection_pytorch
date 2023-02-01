63791f695ac918fc15eb001c645a9e29ed798f8e
Yuriy Surnakov <yurasurnakov@gmail.com>
2023-02-01 21:40:34 +0300

    params of func was changed

--- classification/blue_border/models.py
+++ classification/blue_border/models.py
@@ -1,19 +1,18 @@
 from torchvision.models import resnet50, ResNet50_Weights
 from torch import nn
 
-from classification.config import SUBCLASSES_DICT
 
-def create_model(subclass: str):
+def create_model(num_classes: int):
     """
-    Returns pre-trained ResNet classification model for subclass
+    Returns pre-trained ResNet classification model for subclass.
+    Params:
+        num_classes - number of classes in the current subclass.
     """
     # load pre-trained ResNet50 model
     resnet_model = resnet50(weights=ResNet50_Weights.DEFAULT)
     # get the number of input features
     in_features = resnet_model.fc.in_features
-    # and get the number of output features for current subclass
-    out_features = len(SUBCLASSES_DICT[subclass])
-    # define a new fully-connected layer for the classificator with required number of classes
+    # define a new fully-connected layer for the classifier with required number of classes
     resnet_model.fc = nn.Sequential(
         nn.Linear(in_features, 1024),
         nn.ReLU(),
@@ -21,6 +20,6 @@ def create_model(subclass: str):
         nn.Linear(1024, 256),
         nn.ReLU(),
         nn.Dropout(p=0.5),
-        nn.Linear(256, out_features)
+        nn.Linear(256, num_classes)
     )
     return resnet_model
